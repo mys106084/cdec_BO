cfbe3cbfaa933192ad7a29a1c3290dbbec2eb044
Brian <brian@ubuntu.ubuntu-domain>
2014-06-05 17:20:11 +0100

    alter tran_corpus.py

--- dataprocess/train_corpus.py
+++ dataprocess/train_corpus.py
@@ -1,5 +1,5 @@
 #import sys
-
+'''
 train_url_en = 'corpus.en'
 train_url_es = 'corpus.es'
 train_out = 'corpus.es-en'
@@ -7,18 +7,13 @@ train_out = 'corpus.es-en'
 dev_url_en = 'dev.en'
 dev_url_es = 'dev.es'
 dev_out = 'dev.es-en'
-
-def main():
+'''
+def main(train_url_es,train_url_en,train_out):
 	coutline = 0
 	
 	fin_es = open(train_url_es,'r')
 	fin_en = open(train_url_en,'r')
 	fout = open(train_out,'w')
-	'''
-	fin_es = open(dev_url_es,'r')
-	fin_en = open(dev_url_en,'r')
-	fout = open(dev_out,'w')
-	'''
 
 	while 1:
 		line_en = fin_en.readline()
@@ -34,5 +29,8 @@ def main():
 	fout.close()
 
 if __name__=='__main__':
-  main()
+	if len(sys.argv) != 4:
+		print 'Wrong Parameters'
+		sys.exit(1)
+  	main(sys.argv[1],sys.argv[2],sys.argv[3])
 
